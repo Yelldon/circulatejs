e1c835eb0370e02dfcd8c21ad5670ea4431aa4ab
Greg Patrick <greg.hello@gmail.com>
2020-01-04 20:50:20 -0600

    Add Quill to the project and add it to post pages

---
+++ resources/pages/posts/create.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex w-full">
+    <div class="w-full" ref="editor"></div>
+  </div>
+</template>
+
+<script>
+import Quill from 'quill'
+export default {
+  data() {
+    return {
+      editor: null,
+      options: {
+        // debug: 'info',
+        placeholder: 'Compose an epic...',
+        theme: 'bubble'
+      },
+      data: {
+        text: ''
+      }
+    }
+  },
+  mounted() {
+    this.editor = new Quill(this.$refs.editor, this.options)
+    console.log(this.editor)
+  },
+  watch: {
+    editor: {
+      handler() {
+        this.data.text = this.editor.root.innerHTML
+      },
+      deep: true
+    }
+  }
+}
+</script>
